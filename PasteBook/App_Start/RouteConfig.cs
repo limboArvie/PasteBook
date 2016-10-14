d36b876b1f4b9e79bfc6fb9e595aedde7ba801fa
Bootcamp <Bootcamp@PICCPU-0515>
2016-10-14 17:44:47 +0800

    Added Entity Framework, edited some views

--- PasteBook/App_Start/RouteConfig.cs
+++ PasteBook/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PasteBook
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PasteBook", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ PasteBook/Controllers/PasteBookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PasteBook.Controllers
+{
+    public class PasteBookController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
